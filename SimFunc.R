e9296635dc6986bbf9bb725503fdc11c4b856e04
Weinberger <daniel.weinberger@yale.edu>
2020-12-14 12:11:04 +0100

    update to track source of acquisition

--- SimFunc.R
+++ SimFunc.R
@@ -1,4 +1,5 @@
 #Generate data from a simple transition model
+##States: 0 uncolonized; 1=colonized (non-HH); 2= uncolonized,immune 3=colonized (HH acquistions)
 gen.pair.data<-function(acq.rate.kid= set.acq.rate.kid,
                         acq.rate.adult= set.acq.rate.adult,
                         clear.rate.kid= 1/DurKid,
@@ -14,6 +15,10 @@ gen.pair.data<-function(acq.rate.kid= set.acq.rate.kid,
   ntimes=365
     state<-matrix(NA, ncol=ntimes, nrow=2)
     
+    transmit.kid.parent <- rep(0, ntimes)
+    transmit.parent.kid <- rep(0, ntimes)
+    
+    
     #Row1=kid
     #Row2=parent
     
@@ -26,25 +31,30 @@ gen.pair.data<-function(acq.rate.kid= set.acq.rate.kid,
         state[1,t] <- rbinom(n=1,size=1, prob=(acq.rate.kid ) )
         state[2,t] <- rbinom(n=1,size=1, prob=(acq.rate.adult ) )
         
-      }else if (state[1,t-1]==1 & state[2,t-1]==0 ){ #kid colonized, adult uncolonized
-        state[1,t] <- rbinom(n=1,size=1, prob=(1-clear.rate.kid ) )
+      }else if (state[1,t-1]%in% c(1,3) & state[2,t-1]==0 ){ #kid colonized, adult uncolonized
+        state[1,t] <- state[1,t-1]*rbinom(n=1,size=1, prob=(1-clear.rate.kid ) )
         if(state[1,t]==0){state[1,t]==2} #if transition from col to uncol, go to immune class
         state[2,t] <- rbinom(n=1,size=1, prob=(prob.transmit.kid.day + acq.rate.adult ) )
-       
-      }else if (state[1,t-1]==0 & state[2,t-1]==1 ){ #adult colonized, kid uncolonized
+        
+        if(state[2,t]==1){state[2,t] <-3 } #HH acquistion
+        
+      }else if (state[1,t-1]==0 & state[2,t-1]%in% c(1,3) ){ #adult colonized, kid uncolonized
         state[1,t] <- rbinom(n=1,size=1, prob=( prob.transmit.adult.day + acq.rate.kid ) )
-        state[2,t] <- rbinom(n=1,size=1, prob=(1-clear.rate.adult ) )
+        state[2,t] <- state[2,t-1]*rbinom(n=1,size=1, prob=(1-clear.rate.adult ) )
         if(state[2,t]==0){state[2,t]==2} #if transition from col to uncol, go to immune class
         
-      }else if (state[1,t-1]==1 & state[2,t-1]==1 ){ #kid colonized, adult colonized
-        state[1,t] <- rbinom(n=1,size=1, prob=(1-clear.rate.kid ) )
+        if(state[1,t]==1){state[1,t] <- 3 } #HH acquistion
+        
+        
+      }else if (state[1,t-1]%in% c(1,3) & state[2,t-1]%in% c(1,3) ){ #kid colonized, adult colonized
+        state[1,t] <- state[1,t-1]*rbinom(n=1,size=1, prob=(1-clear.rate.kid ) )
         if(state[1,t]==0){state[1,t]==2} #if transition from col to uncol, go to immune class
         
-        state[2,t] <- rbinom(n=1,size=1, prob=(1 - clear.rate.adult ) )
+        state[2,t] <- state[2,t-1]*rbinom(n=1,size=1, prob=(1 - clear.rate.adult ) )
         if(state[2,t]==0){state[2,t]==2} #if transition from col to uncol, go to immune class
       
       ##IMMUNE CATEGORIES  
-      }else if (state[1,t-1]==2 & state[2,t-1]==1 ){ #kid immune, adult colonized
+      }else if (state[1,t-1]==2 & state[2,t-1] %in% c(1,3) ){ #kid immune, adult colonized
         recover.kid <- rbinom(n=1,size=1, prob=(1 - WaneRateKid ) )
         if(recover.kid){
           state[1,t]=0 #go back to uncolonized
@@ -54,7 +64,7 @@ gen.pair.data<-function(acq.rate.kid= set.acq.rate.kid,
         state[2,t] <- rbinom(n=1,size=1, prob=(1 - clear.rate.adult ) )
         if(state[2,t]==0){state[2,t]==2} #if transition from col to uncol, go to immune class
   
-      }else if (state[1,t-1]==1 & state[2,t-1]==2 ){ #adult immune, kid colonized
+      }else if (state[1,t-1]%in% c(1,3) & state[2,t-1]==2 ){ #adult immune, kid colonized
       recover.adult <- rbinom(n=1,size=1, prob=(1 - WaneRateAdult ) )
       if(recover.adult){
         state[2,t]=0 #go back to uncolonized
@@ -77,6 +87,7 @@ gen.pair.data<-function(acq.rate.kid= set.acq.rate.kid,
       }else{
         state[2,t]=2
       }
+      
     }else if (state[1,t-1]==2 & state[2,t-1]==0 ){ #kid immune, adult uncolonized
       recover.kid <- rbinom(n=1,size=1, prob=(1 - WaneRateKid ) )
       if(recover.kid){
@@ -98,18 +109,15 @@ gen.pair.data<-function(acq.rate.kid= set.acq.rate.kid,
     }
 
     
-
-      
-      
-    
-  
       #If person is colonized (x=1 or uncolonized (0,2))
-    class <- apply(state,2, function(x){
+    #HH States: 1=uncolonized; 2=kid colonized; 3=adult colonized; 4=both colonized
+     HH.States <- apply(state,2, function(x){
       if(x[1] %in% c(0,2) & x[2] %in% c(0,2) ){ z=1}
-      if(x[1]==1 & x[2] %in% c(0,2) ){ z=2}
-      if(x[1] %in% c(0,2) & x[2]==1){ z=3}
-      if(x[1]==1 & x[2]==1){ z=4}
+      if(x[1] %in% c(1,3) & x[2] %in% c(0,2) ){ z=2}
+      if(x[1] %in% c(0,2) & x[2] %in% c(1,3)){ z=3}
+      if(x[1] %in% c(1,3) & x[2] %in% c(1,3)){ z=4}
       return(z)
       })
-  return(class)
+    out.obj <- list('HH.States'=HH.States,'Individual_States'=state)
+  return(out.obj)
 }
--- SimPairs.R
+++ SimPairs.R
@@ -11,15 +11,20 @@ set.acq.rate.kid <- 0.6/365
 init.prev.kid=0.12
 init.prev.adult= init.prev.kid/2
 
-prob.transmit.kid <-1 #prob transmit to HH member
+prob.transmit.kid <-0.5 #prob transmit to HH member
+
 prob.transmit.adult <- prob.transmit.kid/10 #prob transmit to HH member
 
 #Generates simulated data from a Markov model for 365 time points
 # At each time point, can be 1: neither colonized; 2: child colonized; 3: adult colonized; 4: both coonized
 #Each column is a household
-sim.data <- replicate(100,gen.pair.data())
-kid.colonized <- matrix(sim.data %in% c(2,4), ncol=ncol(sim.data))
-adult.colonized <- matrix(sim.data %in% c(3,4), ncol=ncol(sim.data))
+sim.data <- replicate(100,gen.pair.data(), simplify=F)
+
+HH.States <- sapply(sim.data,'[[', 'HH.States')
+Indiv.States <- sapply(sim.data,'[[','Individual_States', simplify='array')
+
+kid.colonized <- matrix(HH.States %in% c(2,4), ncol=ncol(HH.States))
+adult.colonized <- matrix(HH.States %in% c(3,4), ncol=ncol(HH.States))
 
 kid.prevalence <- apply(kid.colonized,1,mean)
 adult.prevalence <- apply(adult.colonized,1,mean)
@@ -30,6 +35,21 @@ abline(h=0.12)
 plot(adult.prevalence)
 abline(h=0.2)
 
+
+#sORT OUT which colonization events are acquired from parent or from 
+
+
 #1=uncolonized; 2=kid colonized; 3=adult colonized; 4=both colonized
-table(sim.data)
+table(HH.States)
+
+
+#Prevalence, by source of acquisition (3=HH, 1=community)
+table(Indiv.States[1,,]) #Kids
+table(Indiv.States[2,,]) #Adults
+
+#Prevalence, by source of acquisition, when colonization detected in both (3=HH, 1=community)
+
+table(Indiv.States[1,,]*(HH.States==4 ) )  #kids
+table(Indiv.States[2,,]*(HH.States==4 ) )  #Adults
+
 
