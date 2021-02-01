ed171c88c91baa06e109f5bca96688cc14dec83d
Weinberger <daniel.weinberger@yale.edu>
2021-02-01 17:13:16 +0100

    dynamic model

--- SimPairs.R
+++ SimPairs.R
@@ -20,44 +20,74 @@ set.acq.rate.adult <- 0.1/365
 set.acq.rate.kid <- 0.6/365
 
 #Probability of transmitting to HH members if colonized
-prob.transmit.kid <-0.5 #prob kid transmits to HH member
+prob.transmit.kid <- 0.5 #prob kid transmits to HH member
 prob.transmit.adult <- prob.transmit.kid/10 #prob adult transmits to HH member
 
 #Generates simulated data from a Markov model for 365 time points
-# At each time point, can be 1: neither colonized; 2: child colonized; 3: adult colonized; 4: both coonized
+# At each time point, can be 1: neither colonized; 2: child colonized; 3: adult colonized; 4: both colonized
 #Each column is a household
-sim.data <- replicate(100,gen.pair.data(), simplify=F)
+sim.data <- replicate(1000,gen.pair.data(), simplify=F)
 
 HH.States <- sapply(sim.data,'[[', 'HH.States')
 Indiv.States <- sapply(sim.data,'[[','Individual_States', simplify='array')
 
-kid.colonized <- matrix(HH.States %in% c(2,4), ncol=ncol(HH.States))
-adult.colonized <- matrix(HH.States %in% c(3,4), ncol=ncol(HH.States))
+# kid.colonized <- matrix(HH.States %in% c(2,4), ncol=ncol(HH.States))
+# adult.colonized <- matrix(HH.States %in% c(3,4), ncol=ncol(HH.States))
+# 
+# kid.prevalence <- apply(kid.colonized,1,mean)
+# adult.prevalence <- apply(adult.colonized,1,mean)
 
-kid.prevalence <- apply(kid.colonized,1,mean)
-adult.prevalence <- apply(adult.colonized,1,mean)
-
-plot(kid.prevalence)
-abline(h=0.12)
-
-plot(adult.prevalence)
-abline(h=0.2)
+# plot(kid.prevalence)
+# abline(h=0.12)
+# 
+# plot(adult.prevalence)
+# abline(h=0.2)
 
 
 #sORT OUT which colonization events are acquired from parent or from 
 
 
-#1=uncolonized; 2=kid colonized; 3=adult colonized; 4=both colonized
-table(HH.States)
+# #1=uncolonized; 2=kid colonized; 3=adult colonized; 4=both colonized
+# table(HH.States)
+# 
+# 
+# #Prevalence, by source of acquisition (3=HH, 1=community)
+# table(Indiv.States[1,,]) #Kids
+# table(Indiv.States[2,,]) #Adults
+# 
+# #Prevalence, by source of acquisition, when colonization detected in both (3=HH, 1=community)
+# 
+# table(Indiv.States[1,,]*(HH.States==4 ) )  #kids
+# table(Indiv.States[2,,]*(HH.States==4 ) )  #Adults
 
-
-#Prevalence, by source of acquisition (3=HH, 1=community)
-table(Indiv.States[1,,]) #Kids
-table(Indiv.States[2,,]) #Adults
-
-#Prevalence, by source of acquisition, when colonization detected in both (3=HH, 1=community)
-
-table(Indiv.States[1,,]*(HH.States==4 ) )  #kids
-table(Indiv.States[2,,]*(HH.States==4 ) )  #Adults
-
-
+# 
+# ## Can we recover this with simple log-binomial regression?
+# #Try to do this at every time point during the year...
+# #day 100; kids col 1, adults col 2
+# dichotomize1  <- Indiv.States
+# dichotomize1[dichotomize1 %in% c(0,2)] <- 0
+# dichotomize1[dichotomize1 %in% c(1,3)] <- 1
+# d60 <- as.data.frame(t(dichotomize1[,100,]))
+# 
+# 
+# names(d60) <- c('kid','adult')
+# ##This effectiveley tests whether the Prob of being colonized in adults is higher when kid is present
+# ##The intercept should give the probability of community-acquired colonized
+# ## exp(bo+b1) gives probability of HH acquired transmission
+# mod1 <- glm(adult ~ kid, family=binomial(link='log'), data=d60)
+# comm.prev <- exp(coef(mod1)['(Intercept)'])
+# hh.prev <-  exp(coef(mod1)['(Intercept)'] + coef(mod1)['kid'] )
+# rr1 <-  exp(coef(mod1)['kid'])
+# actual.irr <- (prob.transmit.kid*set.acq.rate.kid)/set.acq.rate.adult #not sure this is right, butbaseline rate in kids*prob they transmit
+#   
+# 
+# # prob of adult being colonized 
+# #~f((acq rate comm, duration of colonization), (acq rate kid, duration kid, prob kid transmits))
+# #Need ot make a mechanistic model--there are unobserved states, and the observed colonizations are 
+# #from these unobserved states--likelihood would be something like multinomial with 4 groups: nobody colonized, adult colonized, kids colonized, both colonized
+# 
+# mod1 <- glm(adult ~ 1 + kid  , family=binomial(link='log'), data=d60)
+# 
+# ##thought expt
+# acq.rate.kid <- set.acq.rate.kid
+# clear.rate.kid <- 1/DurKid
